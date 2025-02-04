5a2060f75e361cfa0b362527348cc6206d08a3e8
Luis23590232 <l23590232@sjuanrio.tecnm.mx>
2025-02-03 21:09:21 -0600

    Modificaciones (1)

--- app/src/main/java/com/example/testholamundo/MainActivity.kt
+++ app/src/main/java/com/example/testholamundo/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
